e01573b0cbb7b1c71be55a5895591c74c9dfaed9
John Stahre <john.kenneth.kauppi@gmail.com>
2022-01-17 23:16:50 +0100

    lite mer på api

--- APIsocket/index.js
+++ APIsocket/index.js
@@ -28,64 +28,30 @@ app.get("/", (req, res) => {
 
 io.on('connection', (socket) => {
     socket.emit('me', socket.id);
+
+    socket.on('disconnect', () => {
+        socket.broadcast.emit("callended");
+    });
+
+    socket.on("calluser", ({userToCall, signalData, from, name}) => {
+        io.to(userToCall).emit("calluser", {signal: signalData, from, name});
+    });
+    socket.on("answercall", (data) => {
+        io.to(data.to).emit("callaccepted", data.signal);
+    })
 });
 
-socket.on('disconnect', () => {
-    socket.broadcast.emit("callended");
-});
+
 
 // 11:22 in i film
 
-
-server.listen(PORT, () => {
-    console.log('server running on http://localhost:' + PORT);
-});
+server.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
 
 
-
-
-const app = express();
-// kastar in vår express i http server
-const server = http.createServer(app);
-
-
-// Nedan är en middleware för att levera, lägger in sökvägen till mappen public.. Kan vara nån annan för react?
-// Absolut sökväg från root folder, om det pushas upp kommer det vara root 
-// byta ut join
-app.use(express.static(path.join(__dirname, 'public')));
+// server.listen(PORT, () => {
+//     console.log('server running on http://localhost:' + PORT);
 
 
 
 
 
-
-const io = socket(server);
-
-// skapar en eventlistener med ett connection events
-// när någon connectar till vår socket kommer det att finnas info i vår socket.
-
-
-    // När vi har fått ett connection vill vi göra allting som är här innanför.
-    // specifiera vad vi vill göra
-
-    // när vi connectar vill vi emitta och vi vill använda oss av socket emit event
-
-    // socket.emit('message', 'welcome to the John man');
-    // när någon connectar emittas ett meddelande välkommen till chatten som vi sen konsollogga
-
-    // nedan lyssnar vi efter message
-
-    socket.on('message', data => {
-        console.log(data);
-        // nu vill vi skicka till alla sockets och skicka vidare till vår front end
-        io.sockets.emit('message', data);
-    })
-
-    // broadcast skickar till alla andra sockets förutom sin egen
-    // alltså skickar upp till servern och vidare till alla andra förutom sin självt
-    socket.on('typing', data => {
-        socket.broadcast.emit('typing', data)
-
-    })
-
-})
